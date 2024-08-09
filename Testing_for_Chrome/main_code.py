a9da0d81905bc918edc31035a8b518342adadc87
Rohith-Raj05 <rohithraj002912@gmail.com>
2024-08-09 16:39:31 +0530

    adding all the screenshots and code files to the github

---
+++ Testing_for_Chrome/main_code.py
@@ -0,0 +1,116 @@
+
+# Hi sir this is Rohith H and just graduated from PES University under Computer Science Engineering degree  in 2024
+# In this I am demonstrating the First Question of Automation Test 01 UI Testing and this is for the chrome browser
+# The urls and the resolutions are given below
+
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager 
+from webdriver_manager.firefox import GeckoDriverManager 
+import os
+import time
+from datetime import datetime
+import logging
+
+
+
+def get_webdriver(browser):
+    if browser == 'chrome':
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")  # Run in headless mode
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    elif browser == 'firefox':
+        # options = webdriver.FirefoxOptions()
+        # options.add_argument("--headless")  # Run in headless mode
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    else:
+        raise ValueError("Unsupported browser!")
+    return driver
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def take_full_page_screenshot(driver, file_path, width):
+    # Get the height of the entire page
+    web_page_height = driver.execute_script(
+        "return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight,"
+        "document.body.offsetHeight, document.documentElement.offsetHeight,"
+        "document.body.clientHeight, document.documentElement.clientHeight);"
+    )
+    driver.set_window_size(width, web_page_height)  # getting the max height for the website
+
+    # Take a full-page screenshot
+    driver.save_screenshot(file_path)
+
+
+def take_screenshot(url, browser, width, height, idx):
+    driver = None
+    try:
+        driver = get_webdriver(browser)
+        driver.get(url)
+        logging.info(f"Loading {url} on {browser}")
+
+        time.sleep(5)  # Wait for the page to load fully
+
+        folder_type = "Desktop" if width >= 1366 else "Mobile"
+        folder_name = f"Testing_for_chrome/URL{idx}_screenshots/{browser.capitalize()}/{folder_type}/{width}x{height}"
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+
+        timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+        screenshot_path = os.path.join(folder_name, f"{timestamp}.png")
+        take_full_page_screenshot(driver, screenshot_path, width)
+        logging.info(f"Screenshot saved to {screenshot_path}")
+    except Exception as e:
+        logging.error(f"Error taking screenshot for {url} on {browser}: {e}")
+    finally:
+        if driver:
+            driver.quit()
+
+
+def main():
+    urls = [
+        'https://www.getcalley.com/',
+        'https://www.getcalley.com/calley-lifetime-offer/',
+        'https://www.getcalley.com/see-a-demo/',
+        'https://www.getcalley.com/calley-teams-features/',
+        'https://www.getcalley.com/calley-pro-features/'
+    ]
+
+    resolutions = [
+        (1920, 1080),
+        (1366, 768),
+        (1536, 864),
+        (360, 640),
+        (414, 896),
+        (375, 667)
+    ]
+
+    type_of_browser ="chrome" # to indicate on which type of browser we are working on 
+    
+    for idx, url in enumerate(urls, start=1):  #it will start the index from 1 which helps in saving screenshots
+        for width, height in resolutions:
+            take_screenshot(url, type_of_browser , width, height, idx) 
+
+
+if __name__ == "__main__":
+    main()
+
+# now we will run the code
+# it will run for all the urls and all the resolutions and saves the file as seen here 
+# u can see in the terminal that as the screenshot is saved it prints out the log message
+
+
+# u can see from the left thing here it is saving all the images / screenshots based on the url and it is able to seperate and store based on the time stamp
+
+# now url 3
+# it was able to take screenshot of all the given url one by one and saving it on its particular folder as mentioned
+
+# it is going one by one and taking screenshots of all of them
+
+# thank you and i will be making the seperate video for the firefox browser
+
---
+++ Testing_for_Firefox/final_Code.py
@@ -0,0 +1,111 @@
+
+# Hi sir this is Rohith H and just graduated from PES University under Computer Science Engineering degree  in 2024
+# This is the continuation of the previous video where we did for chrome and now we will be doing it for the firefox
+# The urls and the resolutions are given below
+
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager 
+from webdriver_manager.firefox import GeckoDriverManager 
+import os
+import time
+from datetime import datetime
+import logging
+
+
+
+def get_webdriver(browser):
+    if browser == 'chrome':
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")  # Run in headless mode
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    elif browser == 'firefox':
+        # options = webdriver.FirefoxOptions()
+        # options.add_argument("--headless")  # Run in headless mode
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    else:
+        raise ValueError("Unsupported browser!")
+    return driver
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def take_full_page_screenshot(driver, file_path, width):
+    # Get the height of the entire page
+    web_page_height = driver.execute_script(
+        "return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight,"
+        "document.body.offsetHeight, document.documentElement.offsetHeight,"
+        "document.body.clientHeight, document.documentElement.clientHeight);"
+    )
+    driver.set_window_size(width, web_page_height)  # getting the max height for the website
+
+    # Take a full-page screenshot
+    driver.save_screenshot(file_path)
+
+
+def take_screenshot(url, browser, width, height, idx):
+    driver = None
+    try:
+        driver = get_webdriver(browser)
+        driver.get(url)
+        logging.info(f"Loading {url} on {browser}")
+
+        time.sleep(5)  # Wait for the page to load fully
+
+        folder_type = "Desktop" if width >= 1366 else "Mobile"
+        folder_name = f"Testing_for_Firefox/URL{idx}_screenshots/{browser.capitalize()}/{folder_type}/{width}x{height}"
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+
+        timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+        screenshot_path = os.path.join(folder_name, f"{timestamp}.png")
+        take_full_page_screenshot(driver, screenshot_path, width)
+        driver.save_full_page_screenshot(screenshot_path)
+        logging.info(f"Screenshot saved to {screenshot_path}")
+    except Exception as e:
+        logging.error(f"Error taking screenshot for {url} on {browser}: {e}")
+    finally:
+        if driver:
+            driver.quit()
+
+
+def main():
+    urls = [
+        'https://www.getcalley.com/',
+        'https://www.getcalley.com/calley-lifetime-offer/',
+        'https://www.getcalley.com/see-a-demo/',
+        'https://www.getcalley.com/calley-teams-features/',
+        'https://www.getcalley.com/calley-pro-features/'
+    ]
+
+    resolutions = [
+        (1920, 1080),
+        (1366, 768),
+        (1536, 864),
+        (360, 640),
+        (414, 896),
+        (375, 667)
+    ]
+
+    type_of_browser ="firefox" # to indicate on which type of browser we are working on 
+    
+    for idx, url in enumerate(urls, start=1):  #it will start the index from 1 which helps in saving screenshots
+        for width, height in resolutions:
+            take_screenshot(url, type_of_browser , width, height, idx) 
+
+
+if __name__ == "__main__":
+    main()
+
+# now we will run the code
+# now in this we are running without headless mode so that there will be some kind of differentiation between chrome and firefox
+
+# It is quite similar to the chrome but in this we are using firefox that is the only difference
+
+#  as u can see from the left side we have done the testing for all the 5 websites and for all resolutions
+
+# Thanks You
